bea40b37f5884d8b8bfafb4753a0f3dbc92511f6
solomonirving <solomon.irving@gmail.com>
2021-07-09 15:32:08 -0700

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import pic from '../img/millennium_falcon_blast_off.jpeg';
-import pic2 from '../img/millennium_falcon_blast_off.jpeg';
+import pic2 from '../img/battle_of_endor.png';
 
 
 export default function Home() {
     return (
   /* background: url("./img/millennium_falcon_blast_off.jpeg") no-repeat center top; */
         <div className="home">
-            <img src={pic}  alt={pic2} srcset="" />
+            <img src={pic2}  alt={pic2} srcset="" />
         </div>
     )
 }
